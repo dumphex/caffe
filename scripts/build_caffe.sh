0c9d6a6cf532a14eaa7416230e487e11f40159a7
dumphex <dumphex@163.com>
2020-03-29 21:55:55 +0800

    Add build_caffe.sh script.

---
+++ scripts/build_caffe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+top=${SCRIPT_DIR}/../ 
+
+build_dir=${top}/build
+rm -rf ${build_dir}
+mkdir ${build_dir}
+
+pushd ${build_dir}
+cmake -DCMAKE_BUILD_TYPE=Release ../ && make -j`nproc` && make install
+popd
